deaa54f6d15f4ce6253fa8a11b3ed8129b10e8ac
Najel Alarcon <najelalarcon@gmail.com>
2024-03-27 17:42:33 -0700

    Start of app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, SafeAreaView, StatusBar } from 'react-native';
+import { Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <Text>Hello App</Text>
+  );
+};
+
+
+export default App;
